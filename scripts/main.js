6892879444c25559fb0e106acb025eff235ab558
evgeniy88-kram <gedata88@gmail.com>
2021-02-07 12:15:20 +0200

    new

--- scripts/main.js
+++ scripts/main.js
@@ -67,7 +67,8 @@
         //
         //Modal basket
         let modalBasket = $('.modal-basket, .dark-background');
-        $('.header__logo + svg').on('click', function () {
+        let btnOpenModal = $('.header__logo + svg');
+        btnOpenModal.on('click', function () {
             modalBasket.show();
         });
         $('.modal-basket__close').on('click', function () {
@@ -86,23 +87,31 @@
             }
         });
         //
-    //    Adding to bag
+        //Adding to bag
         let baskCont = [],
             bagsInfo = [
-                {name: 'American Tourister Crete Polyester 770 mm Blue', price: 45.00},
-                {name: 'American Tourister Crete Polyester 680 mm Black', price: 57.00},
-                {name: 'Nasher Miles Manarola Soft-Sided Luggage Set', price: 60.00},
-                {name: 'Nasher Miles Santorini PP Hard-Sided Luggage Set', price: 67.00},
-                {name: 'Nasher Miles Santorini PP Hard-Sided Check-In Luggage Bag', price: 73.00},
-                {name: 'American Tourister X-Bags Casual 2 Fabric', price: 79.00}
-            ],
-            campsInfo = [
-                {name: 'camp1', price: 45.00},
-                {name: 'camp', price: 57.00},
-                {name: 'camp2', price: 60.00},
-            ],
-            camerasInfo = [],
-            lensInfo = []
+                {name: 'American Tourister Crete Polyester 770 mm Blue', price: 45.00, quantity: 1},
+                {name: 'American Tourister Crete Polyester 680 mm Black', price: 57.00, quantity: 1},
+                {name: 'Nasher Miles Manarola Soft-Sided Luggage Set', price: 60.00, quantity: 1},
+                {name: 'Nasher Miles Santorini PP Hard-Sided Luggage Set', price: 67.00, quantity: 1},
+                {name: 'Nasher Miles Santorini PP Hard-Sided Check-In Luggage Bag', price: 73.00, quantity: 1},
+                {name: 'American Tourister X-Bags Casual 2 Fabric', price: 79.00, quantity: 1}
+            ]
+            // campsInfo = [
+            //     {name: 'Diswa Quick Setup 4-Person All Season', price: 67.00, quantity: 1},
+            //     {name: 'Dealcrox New 2 Person Tent', price: 67.00, quantity: 1},
+            //     {name: 'Ozoy Picnic Camping Portable Waterproof Tent', price: 67.00, quantity: 1}
+            // ],
+            // camerasInfo = [
+            //     {name: 'Basics 60-Inch Lightweight Tripod with Bag (Black)', price: 212.00, quantity: 1},
+            //     {name: 'Photron Tripod Stedy 450 with 4.5 Feet Pan Head', price: 240.00, quantity: 1},
+            //     {name: 'SONY G7 Mirrorless Camera with LUMIX G VARIO 14-42mm - Black', price: 450.00, quantity: 1}
+            // ],
+            // lensInfo = [
+            //     {name: 'Tamron A17E AF 70-300mm F/4-5.6 Di LD Macro Telephoto Zoom Lens (Black)', price: 221.00, quantity: 1},
+            //     {name: 'Nikon AF 70-300mm f/4-5.6G Telephoto Zoom Lens', price: 327.00, quantity: 1},
+            //     {name: 'Yongnuo_ EF YN 50mm F/1.8 1:1.8 Standard Prime Lens', price: 450.00, quantity: 1}
+            //     ];
 
         $('.bags__grid__item:nth-child(1) svg').one('click', function(){
             baskCont.push(bagsInfo[0]);
@@ -122,26 +131,28 @@
         $('.bags__grid__item:nth-child(6) svg').one('click', function(){
             baskCont.push(bagsInfo[5]);
         });
-        $('.header__logo + svg').on('click', function(){
-            $('.modal-basket ol').append('<li></li>');
-            $('.modal-basket ol li')
-                .text(`${baskCont[0].name} - ${baskCont[0].quantity} шт. - ${baskCont[0].quantity * baskCont[0].price} $`);
-            $('.modal-basket ol').append('<li></li>');
-            $('.modal-basket ol li')
-                .text(`${baskCont[1].name} - ${baskCont[1].quantity} шт. - ${baskCont[1].quantity * baskCont[1].price} $`);                ;
-            $('.modal-basket ol').append('<li></li>');
-            $('.modal-basket ol li')
-                .text(`${baskCont[2].name} - ${baskCont[2].quantity} шт. - ${baskCont[2].quantity * baskCont[2].price} $`);
-            $('.modal-basket ol').append('<li></li>');
-            $('.modal-basket ol li')
-                .text(`${baskCont[3].name} - ${baskCont[3].quantity} шт. - ${baskCont[3].quantity * baskCont[3].price} $`);
-            $('.modal-basket ol').append('<li></li>');
-            $('.modal-basket ol li')
-                .text(`${baskCont[4].name} - ${baskCont[4].quantity} шт. - ${baskCont[4].quantity * baskCont[4].price} $`);
-            $('.modal-basket ol').append('<li></li>');
-            $('.modal-basket ol li')
-                .text(`${baskCont[5].name} - ${baskCont[5].quantity} шт. - ${baskCont[5].quantity * baskCont[5].price} $`);
+        btnOpenModal.one('click', function(){
+            for (i = 1; i < 7; i++) {
+                $('.modal-basket__content section').append('<div></div>');
+            }
+            $('.modal-basket__content section div:nth-child(1)').text(`${baskCont[0].name}`);
+            $('.modal-basket__content section div:nth-child(2)').text(`${baskCont[0].price} $`);
+            $('.modal-basket__content section div:nth-child(3)').css('background', '#3ACDD5').text(`-`);
+            $('.modal-basket__content div:nth-child(4)').text(`${baskCont[0].quantity} шт`);
+            $('.modal-basket__content div:nth-child(5)').css('background', '#3ACDD5').text(`+`);
+            $('.modal-basket__content div:nth-child(6)').text(`${baskCont[0].quantity * baskCont[0].price} $`);
         });
+        $('.modal-basket__content section div:nth-child(3)').on('click', function (){
+            if (baskCont[0].quantity > 1) {
+                baskCont[0].quantity -= 1;
+            }
+        });
+        $('.modal-basket__content section div:nth-child(5)').on('click', function (){
+                baskCont[0].quantity++;
+            $('.modal-basket__content div:nth-child(4)').text(`${baskCont[0].quantity} шт`);
+            console.log(baskCont[0].quantity);
+        });
+
     });
 })(jQuery);
 
